152b25d7ac038846b87cc0cfa13c154392f39f40
DangHuynh <danghuynh.dev@gmail.com>
2022-03-19 20:05:45 +0700

    fix database

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 async function connect(){
     try {
-        await mongoose.connect('mongodb://localhost:27017/gamecaro');
+        const MONGO_URI = 'mongodb+srv://danghuynh:dang1810@cluster0.meq5z.mongodb.net/gamecaro?retryWrites=true&w=majority';
+        await mongoose.connect(MONGO_URI,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
         console.log("connect success!!!");
     } catch (error) {
         console.log("connect fail!!!");
